dab0d674d5af7238ad284bf7b7ee43b099869eda
tanishka-web <83286129+tanishka-web@users.noreply.github.com>
2021-06-22 17:51:19 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,32 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+    this.remove=true
+ //this.Visiblity=255;
+  }
+display()
+{
+  //console.log(this.body.)
+if((this.body.speed)<2.5)
+{super.display();
+}else
+{
+World.remove(world,this.body) 
+if(this.remove===true) {
+  score=score+50;
+  this.remove=false;
+}
+
+
+//push();
+//this.Visiblity=this.Visiblity-5;
+//tint(255,this.Visiblity)
+
+//do nothing
+//image(this.image,this.body.position.x,this.body.position.y,50,50)
+//pop();
+}}
+};
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# activitiy-33
+# AngryBirdsStage3
+Stage 3 Angry Birds: Introducing Constraint
